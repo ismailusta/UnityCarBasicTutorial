3e9f4cd017326210807fe2482b4cceda69737f7b
ismailusta <ismail.usta028@gmail.com>
2025-05-07 21:55:19 +0300

    .

---
+++ BasicGamePlay/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10f;
+    public float range;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -range)
+        {
+            transform.position = new Vector3(-range, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > range)
+        {
+            transform.position = new Vector3(range, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}

5645bcd525a27868f9e258049e52e6208d83166f
ismailusta <ismail.usta028@gmail.com>
2025-05-10 17:33:07 +0300

    .

--- BasicGamePlay/Assets/Scripts/PlayerController.cs
+++ BasicGamePlay/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float speed = 10f;
     public float range;
+    public GameObject pizzaPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +15,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(pizzaPrefab, transform.position, pizzaPrefab.transform.rotation);
+        }
         if (transform.position.x < -range)
         {
             transform.position = new Vector3(-range, transform.position.y, transform.position.z);
---
+++ BasicGamePlay/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
+    public float startDelay = 2;
+    public float repeatRate = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
